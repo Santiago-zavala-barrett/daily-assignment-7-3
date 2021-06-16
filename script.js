e74dfa4705236ab62fe3e93098bcb6cb567cced7
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-16 13:21:14 +0000

    fixing errors in java

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let myAge = 25;
-("How many years old are you?")
-if (myAge >= 17) {
-  console.log("You can see an R rated movie alone")
-} else if (myAge >= 13) {
-  console.log("You can see PG-13 rated movie alone")
-} else if (myAge >= 5) {
-  console.log("You can see PG or G rated movie alone")
+let Const = 25;('How many years old are you?')
+if (Const >= 17) {
+  console.log('You can see an R rated movie alone')
+} else if (Const >= 13) {
+  console.log('You can see PG-13 rated movie alone')
+} else if (Const >= 5) {
+  console.log('You can see PG or G rated movie alone')
 } else {
-  console.log("You are to young to watch any movies on your own")
+  console.log('You are to young to watch any movies on your own')
 }
-console.log("Thank you for taking your time to verify your age!")
+console.log('Thank you for taking your time to verify your age!')
