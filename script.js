31ef0c37fdcef9665ecdd06eb2f73f1b4d4d6745
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-16 13:10:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let myAge = 25;
+("How many years old are you?")
+if (myAge >= 17) {
+  console.log("You can see an R rated movie alone")
+} else if (myAge >= 13) {
+  console.log("You can see PG-13 rated movie alone")
+} else if (myAge >= 5) {
+  console.log("You can see PG or G rated movie alone")
+} else {
+  console.log("You are to young to watch any movies on your own")
+}
+console.log("Thank you for taking your time to verify your age!")
